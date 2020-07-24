4ad98d27f41521a87b39bc84dab71091c0fc0e2c
Matias Ferrari <matiasferrari478@gmail.com>
2020-07-24 02:52:55 -0300

    ejercicios de IF y ferreteIluminacion

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,5 +1,79 @@
 function mostrar()
 {
+	/*Se pide ingresar una edad y un estado cicvil
+	a todos los mayores de edad, mostrar el mensaje "se responsable"
+	a todos los menores de edad, mostrar el mensaje "respeta a tus mayores"
+	a los adultos mayores( mas de 60 años) tambien informar "sos persona de riesgo"
+	a los niños tambien informar "hagan la tarea" 
+	a los adolescentes casado o Divorciado " sos muy joven para no ser soltero"
+	a los mayores de edad solteros, tambien informar "a vivir la vida"
+	a los mayores de edad casados , tambien informar "a disfrutar la pareja"
+	a los divorciados informar tambien "a intentarlo nuevamente"
+	*/
+
+	var edadIngresada;
+	var estadoDeCivil;
+	
+	edadIngresada=txtIdEdad.value;
+	edadIngresada=parseInt(edadIngresada);
+
+	estadoDeCivil=estadoCivil.value;
+	
+	if(edadIngresada>18)
+	{
+		alert("Se responsable");
+	
+		if(edadIngresada>59)
+		{
+			alert("Ya que sos persona de alto riesgo");
+		}
+		if(estadoDeCivil=="Casado")
+		{
+			alert("a disfrutar la pareja");	
+		}else
+		{
+			if(estadoDeCivil=="Soltero")
+			{
+				alert("a vivir la vida");
+			}
+			else
+			{
+				if(estadoDeCivil=="Divorciado")
+				{
+					alert("a intentarlo nuevamente");
+				}	
+			}
+		}
+						
+	}
+	else
+	{
+		if(edadIngresada<18)
+		{
+			alert("Respeta a tus mayores");
+					
+			if(edadIngresada<13)
+			{
+				alert("hagan la tarea");
+			}
+			if(estadoDeCivil!="Soltero")
+			{
+				alert("es mu pequeño para no ser soltero");
+			}
+		}
+	
+	}		
+	
+	
+	/*if(edadIngresada>60)
+	{
+		alert("Se responsable ya que sos persona de alto riesgo");
+	}*/
+	
+	
+	
+	
+	/*sEJERCICIO 8
 	var edadIngresada;
 	var estadou;
 
@@ -19,7 +93,5 @@ function mostrar()
 			alert("Es soltero y no es menor");
 		}
 	}
-
-
-
+	*/
 }//FIN DE LA FUNCIÓN
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,4 +1,6 @@
-/*4.	Para el departamento de iluminación:
+/*Matias Ferrari !!!
+
+4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -8,7 +10,238 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
+ function CalcularPrecio () 
 {
- 	
+    var cantDeLamparas;
+    var marcaDeLamparas;
+    var precioBrutoDeLasLamparas;
+    var porcentajeDeDescuento;
+    var descuento;
+    var precioFinal;
+    var aumento10;
+    var precioFinalConAumento;
+
+    porcentajeDeDescuento=0;
+    cantDeLamparas=txtIdCantidad.value;
+    cantDeLamparas=parseInt(cantDeLamparas);
+
+    marcaDeLamparas=Marca.value;
+
+    precioBrutoDeLasLamparas=cantDeLamparas*35;
+    //descuento=precioBrutoDeLasLamparas*porcentajeDeDescuento/100;
+    //precioFinal=precioBrutoDeLasLamparas-descuento;
+
+   if(cantDeLamparas>5)
+   {
+       porcentajeDeDescuento=50;
+   }else
+   {
+       if(cantDeLamparas==5)
+       {
+            if(marcaDeLamparas=="ArgentinaLuz")
+            {
+                porcentajeDeDescuento=40;
+            }else
+            {
+                porcentajeDeDescuento=30;
+            }
+       }else
+       {
+           if(cantDeLamparas==4)
+           {
+               if(marcaDeLamparas=="ArgentinaLuz" || marcaDeLamparas=="FelipeLamparas")
+               {
+                    porcentajeDeDescuento=25;
+               }else
+               {
+                   porcentajeDeDescuento=20;
+               }
+           }else
+           {
+               if(cantDeLamparas==3)
+               {
+                   if(marcaDeLamparas=="ArgentinaLuz")
+                   {
+                        porcentajeDeDescuento=15;
+                   }else
+                   {
+                        if(marcaDeLamparas=="FelipeLamparas")
+                        {
+                            porcentajeDeDescuento=10;
+                        }else
+                        {    
+                            if(marcaDeLamparas!="ArgentinaLuz" && "FelipeLamparas")
+                            {
+                                porcentajeDeDescuento=5;
+                            }
+                        }
+                   }
+               }
+           }
+       }
+   }
+    
+    
+    
+    
+    descuento=precioBrutoDeLasLamparas*porcentajeDeDescuento/100;
+    precioFinal=precioBrutoDeLasLamparas-descuento;
+    txtIdprecioDescuento.value=precioFinal;
+
+    aumento10=precioBrutoDeLasLamparas*10/100;
+    precioFinalConAumento=precioFinal+aumento10;
+    
+    
+    if(precioFinal>120)
+    {
+        alert("Usted pago "+aumento10+" de IIBB, siendo el precio final "+precioFinalConAumento+" con el impuesto que se pagó");
+        txtIdprecioDescuento.value=precioFinalConAumento;
+    }
+
+   // aumento10=precioBrutoDeLasLamparas*10/100;
+    //precioFinalConAumento=precioFinal+aumento10;
+
+
 }
+/*
+ 
+ function CalcularPrecio ()         !!PRIMER INTENTO FALLIDO!!
+{
+    var cantDeLamparas;
+    var marcaDeLamparas;
+    var precioBrutoDeLasLamparas;
+    var porcentajeDeDescuento;
+    var descuento;
+    var precioFinal;
+
+    porcentajeDeDescuento=0;
+    cantDeLamparas=txtIdCantidad.value;
+    cantDeLamparas=parseInt(cantDeLamparas);
+
+    marcaDeLamparas=Marca.value;
+
+    precioBrutoDeLasLamparas=cantDeLamparas*35;
+    //descuento=precioBrutoDeLasLamparas*porcentajeDeDescuento/100;
+    //precioFinal=precioBrutoDeLasLamparas-descuento;
+
+    if(cantDeLamparas>5)
+    {
+        porcentajeDeDescuento=50;
+
+    }else
+    {
+        if(cantDeLamparas==5 && marcaDeLamparas=="ArgentinaLuz")
+        {
+            porcentajeDeDescuento=40;
+        }else
+        {
+            porcentajeDeDescuento=30;
+            
+            if(cantDeLamparas==4 && marcaDeLamparas=="ArgentinaLuz" || marcaDeLamparas=="FelipeLamparas")
+            {
+                porcentajeDeDescuento=25;
+
+                
+            }else
+            {
+                porcentajeDeDescuento=20;
+                
+                if(cantDeLamparas==3 && marcaDeLamparas=="ArgentinaLuz")
+                {
+                    porcentajeDeDescuento=15;
+                }else
+                {
+                    if(cantDeLamparas==3 && marcaDeLamparas=="FelipeLamparas")
+                    {
+                        porcentajeDeDescuento=10;
+                    }else
+                    {
+                        porcentajeDeDescuento=5;
+
+                    }
+                }
+            }
+        
+        }
+        
+    }
+    
+
+ function CalcularPrecio ()        !!SEGUNDO INTENTO NO ANDA BIEN NI EL B NI EL C!!
+ {
+     var cantDeLamparas;
+     var marcaDeLamparas;
+     var precioBrutoDeLasLamparas;
+     var porcentajeDeDescuento;
+     var descuento;
+     var precioFinal;
+ 
+     porcentajeDeDescuento=0;
+     cantDeLamparas=txtIdCantidad.value;
+     cantDeLamparas=parseInt(cantDeLamparas);
+ 
+     marcaDeLamparas=Marca.value;
+ 
+     precioBrutoDeLasLamparas=cantDeLamparas*35;
+     //descuento=precioBrutoDeLasLamparas*porcentajeDeDescuento/100;
+     //precioFinal=precioBrutoDeLasLamparas-descuento;
+ 
+     if(cantDeLamparas>5)
+     {
+         porcentajeDeDescuento=50;
+ 
+     }else
+     {
+         if(cantDeLamparas==5 && marcaDeLamparas=="ArgentinaLuz")
+         {
+             porcentajeDeDescuento=40;
+         }else
+         {
+             porcentajeDeDescuento=30;
+             
+                 if(cantDeLamparas==4 && marcaDeLamparas=="ArgentinaLuz" || marcaDeLamparas=="FelipeLamparas")
+                 {
+                     porcentajeDeDescuento=25;
+                 }else
+                 {
+                     porcentajeDeDescuento=20;
+                         
+                         if(cantDeLamparas==3 && marcaDeLamparas=="ArgentinaLuz")
+                         {
+                             porcentajeDeDescuento=15;
+                         }else
+                         {
+                             if(cantDeLamparas==3 && marcaDeLamparas=="FelipeLamparas")
+                             {
+                                 porcentajeDeDescuento=10;
+                             }else
+                             {
+                                 if(cantDeLamparas==3)
+                                 {
+                                     porcentajeDeDescuento=5;
+                                 }
+                                 
+                             }    
+                     
+                         }      
+                 }
+         
+         }
+         
+     }
+     
+     
+     
+     
+     
+     
+     descuento=precioBrutoDeLasLamparas*porcentajeDeDescuento/100;
+     precioFinal=precioBrutoDeLasLamparas-descuento;
+     txtIdprecioDescuento.value=precioFinal;
+ 
+ 
+ 
+ 
+ 
+ }
+ */
